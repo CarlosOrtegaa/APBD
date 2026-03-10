80014951c8ffe127f72d5aba17dd79a7434dcec0
Carlos Ortega <s999635@pjwstk.edu.pl>
2026-03-10 12:52:20 +0100

    Add min calculation

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ double[] numbers = { 5, 10, 15, 20 };
 
 double average = numbers.Average();
 double max = numbers.Max();
+double min = numbers.Min();
 
 Console.WriteLine($"Average: {average}");
 Console.WriteLine($"Max: {max}");
+Console.WriteLine($"Min: {min}");
