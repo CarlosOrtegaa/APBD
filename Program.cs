1503eb8a8e0e3803002cb232f9b4ba61f9672539
Carlos Ortega <s999635@pjwstk.edu.pl>
2026-03-10 12:53:15 +0100

    Change Output labels

--- Program.cs
+++ Program.cs
@@ -5,5 +5,5 @@ double[] numbers = { 5, 10, 15, 20 };
 double average = numbers.Average();
 double max = numbers.Max();
 
-Console.WriteLine($"Average: {average}");
-Console.WriteLine($"Max: {max}");
+Console.WriteLine($"Average value: {average}");
+Console.WriteLine($"Maximum value: {max}");
