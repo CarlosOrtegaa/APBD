271f62266ddd244b665513a6519d7e579616d9e5
Carlos Ortega <s999635@pjwstk.edu.pl>
2026-03-10 12:49:58 +0100

    Add max calculation

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
-﻿double[] numbers = { 5, 10, 15, 20 };
+﻿using System.Linq;
+
+double[] numbers = { 5, 10, 15, 20 };
 
 double average = numbers.Average();
+double max = numbers.Max();
 
 Console.WriteLine($"Average: {average}");
+Console.WriteLine($"Max: {max}");
